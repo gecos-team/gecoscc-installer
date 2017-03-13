4fe035649e93b3ce4d94bf292dd16f004c6b2cfe
Diego Martinez Castaneda <dmartinez@solutia-it.es>
2017-03-13 17:35:58 +0100

    chef 12 uses the FQDN of the server for getting the certificate

--- gecoscc-installer.sh
+++ gecoscc-installer.sh
@@ -28,7 +28,7 @@ export MONGO_URL="mongodb://localhost:27017/gecoscc"
 export CHEF_SERVER_VERSION="12.6.0"
 export CHEF_SERVER_PACKAGE_URL="https://packages.chef.io/stable/el/6/chef-server-core-$CHEF_SERVER_VERSION-1.el6.x86_64.rpm"
 export CHEF_CLIENT_PACKAGE_URL="https://packages.chef.io/stable/el/6/chef-$CHEF_SERVER_VERSION-1.el6.x86_64.rpm"
-export CHEF_SERVER_URL="https://localhost/"
+export CHEF_SERVER_URL="https://`hostname -f`/"
 export CHEF_SUPERADMIN_USER="pivotal"
 export CHEF_SUPERADMIN_CERTIFICATE="/etc/opscode/pivotal.pem"
 
