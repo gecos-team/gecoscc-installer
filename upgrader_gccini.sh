07ef0d7409c4239e15ab7202f33a75bef41676ac
Diego Martinez Castaneda <n1mh@n1mh.org>
2017-10-23 13:07:08 +0200

    added more checkings and translated to English

--- upgrader_gccini.sh
+++ upgrader_gccini.sh
@@ -9,67 +9,77 @@ GCCVER="2.2.0"
 CHEFPK="https://packages.chef.io/files/stable/chef/13.5.3/el/6/chef-13.5.3-1.el6.x86_64.rpm"
 CHEFCL="chef-13.5.3-1.el6.x86_64"
 
+function processGecosccini() {
+    echo -n "backing up $GCCINI on $GCCINI-$DATE.backup... "
+    cp -f $GCCINI $GCCINI-$DATE.backup
+    echo 'done.'
+
+    if [ `grep -c "http://forja.guadalinex.org/webs/gecos/doc/v2/doku.php" $GCCINI` -eq '1' ] ; then
+        echo -n "found wrong help URL --> changing... " && \
+        sed -i 's|^help_manual_url = http://forja.guadalinex.org/webs/gecos/doc/v2/doku.php|help_manual_url = https://github.com/gecos-team/gecos-doc/wiki/Politicas:|' $GCCINI && \
+        echo 'done.'
+    fi
+
+    if [ `grep -c "^worker_class = gevent" $GCCINI` -eq '1' ] ; then
+        echo -n "found wrong gevent definition --> changing... " && \
+        sed -i 's/^worker_class = gevent/worker_class = gecoscc.socks.GecosGeventSocketIOWorker/' $GCCINI && \
+        echo 'done.'
+    fi
+
+    if [ `grep -c "v2.gecos.guadalinex.org/gems" $GCCINI` -eq '1' ] ; then
+        echo -n "found wrong gems repository --> changing... " && \
+        sed -i 's|^firstboot_api.gem_repo = http://v2.gecos.guadalinex.org/gems|firstboot_api.gem_repo = http://v3.gecos.guadalinex.org/gems|' $GCCINI && \
+        echo 'done.'
+    fi
+
+    if [ `grep -c "^timeout = 600" $GCCINI` -eq '1' ] ; then
+        echo -n "found wrong timeout --> changing... " && \
+        sed -i 's/^timeout = 600/timeout = 1800/' $GCCINI && \
+        echo 'done.'
+    fi
+
+    if [ `grep -c "http://v3.gecos.guadalinex.org/gecos/" $GCCINI` -eq '0' ] ; then
+        echo -n "found no Guadalinex v3 repos --> changing... " && \
+        sed -i 's|"http://v2.gecos.guadalinex.org/gecos/",|"http://v2.gecos.guadalinex.org/gecos/", "http://v3.gecos.guadalinex.org/gecos/",|' $GCCINI && \
+        sed -i 's|"http://v2.gecos.guadalinex.org/ubuntu/",|"http://v2.gecos.guadalinex.org/ubuntu/", "http://v3.gecos.guadalinex.org/ubuntu/",|' $GCCINI && \
+        sed -i 's|"http://v2.gecos.guadalinex.org/mint/"|"http://v2.gecos.guadalinex.org/mint/", "http://v3.gecos.guadalinex.org/mint/"|' $GCCINI && \
+        echo 'done.'
+    fi
+
+    if [ `grep -c "$GECOS_VERSION" $GCCINI` -gt '0' ] ; then
+        echo -n "found \$GECOS_VERSION variable --> changing... " && \
+        sed -i "s/\${GECOS_VERSION}/$GCCVER/g" $GCCINI && \
+        echo 'done.'
+    fi
+}
+
 if [ ! $(id -u) = 0 ]; then
-    echo 'ERROR: debe ejecutar este script como root'
+    echo 'ERROR: you must be root to run this script'
     exit 1
 fi
 
-echo -n "haciendo backup de $GCCINI en $GCCINI-$DATE.backup... "
-cp -f $GCCINI $GCCINI-$DATE.backup
-echo "hecho."
-
-if [ `grep -c "http://forja.guadalinex.org/webs/gecos/doc/v2/doku.php" $GCCINI` -eq '1' ] ; then
-    echo -n "encontrado enlace a ayuda incorrecto --> cambiando... " && \
-    sed -i 's|^help_manual_url = http://forja.guadalinex.org/webs/gecos/doc/v2/doku.php|help_manual_url = https://github.com/gecos-team/gecos-doc/wiki/Politicas:|' $GCCINI && \
-    echo "hecho."
-fi
-
-if [ `grep -c "^worker_class = gevent" $GCCINI` -eq '1' ] ; then
-    echo -n "encontrado gevent incorrecto --> cambiando... " && \
-    sed -i 's/^worker_class = gevent/worker_class = gecoscc.socks.GecosGeventSocketIOWorker/' $GCCINI && \
-    echo "hecho."
-fi
-
-if [ `grep -c "v2.gecos.guadalinex.org/gems" $GCCINI` -eq '1' ] ; then
-    echo -n "encontrado repo gemas incorrecto --> cambiando... " && \
-    sed -i 's|^firstboot_api.gem_repo = http://v2.gecos.guadalinex.org/gems|firstboot_api.gem_repo = http://v3.gecos.guadalinex.org/gems|' $GCCINI && \
-    echo "hecho."
-fi
-
-if [ `grep -c "^timeout = 600" $GCCINI` -eq '1' ] ; then
-    echo -n "encontrado timeout incorrecto --> cambiando... " && \
-    sed -i 's/^timeout = 600/timeout = 1800/' $GCCINI && \
-    echo "hecho."
-fi
-
-if [ `grep -c "http://v3.gecos.guadalinex.org/gecos/" $GCCINI` -eq '0' ] ; then
-    echo -n "no se ha encontrado repos v3 --> cambiando... " && \
-    sed -i 's|"http://v2.gecos.guadalinex.org/gecos/",|"http://v2.gecos.guadalinex.org/gecos/", "http://v3.gecos.guadalinex.org/gecos/",|' $GCCINI && \
-    sed -i 's|"http://v2.gecos.guadalinex.org/ubuntu/",|"http://v2.gecos.guadalinex.org/ubuntu/", "http://v3.gecos.guadalinex.org/ubuntu/",|' $GCCINI && \
-    sed -i 's|"http://v2.gecos.guadalinex.org/mint/"|"http://v2.gecos.guadalinex.org/mint/", "http://v3.gecos.guadalinex.org/mint/"|' $GCCINI && \
-    echo "hecho."
-fi
-
-if [ `grep -c "$GECOS_VERSION" $GCCINI` -gt '0' ] ; then
-    echo -n "encontrada variable \$GECOS_VERSION --> cambiando... " && \
-    sed -i "s/\${GECOS_VERSION}/$GCCVER/g" $GCCINI && \
-    echo "hecho."
+if [ ! -f $GCCINI ] ; then
+    echo "ERROR: $GCCINI not found"
+    echo 'Is this a real GECOSCC server?'
+    exit 2
+else
+    processGecosccini
 fi
 
 if [ x`rpm -qa chef` != x$CHEFCL ] ; then
-    echo "no encontrado cliente chef 13.5.3 --> instalando... " && \
+    echo "chef package not found, installing version 13.5.3 --> installing... " && \
     rpm -Uvh $CHEFPK && \
-    echo "hecho."
+    echo 'done.'
 fi
 
 if [ -x $GCCDIR/bin/pmanage ] ; then
-    echo "lanzando la sincronización de listas de paquetes..."
+    echo 'upgrading lists of packages...'
     [ -f /opt/rh/python27/enable ] && source /opt/rh/python27/enable
     [ -f $GCCDIR/bin/activate ]    && source $GCCDIR/bin/activate
     $GCCDIR/bin/pmanage $GCCINI synchronize_repositories
-    echo "hecho."
+    echo 'done.'
 else
-    echo "AVISO: no se ha lanzado la sincronización de listas de paquetes porque no hay fichero pmanage."
+    echo "WARNING: packages list upgrade hasn't been done because there is no pmanage executable"
 fi
 
 exit 0
