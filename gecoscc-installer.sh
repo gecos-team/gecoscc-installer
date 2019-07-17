54071fc72a4fb4ca558641de0cbf3db3f6d920c5
Alfonso E.M <alfonso@el-magnifico.org>
2019-07-17 13:44:04 +0200

    Control Center installation ok

--- gecoscc-installer.sh
+++ gecoscc-installer.sh
@@ -15,15 +15,18 @@ set -u
 set -e
 set +o nounset
 
+ADMIN_USER=superadmin
+ADMIN_EMAIL=test@test.com
+
 export CHEF_SERVER_VERSION="12.16.9"
-export CHEF_SERVER_PACKAGE_URL="https://packages.chef.io/files/stable/chef-server/$CHEF_SERVER_VERSION/el/6/chef-server-core-$CHEF_SERVER_VERSION-1.el6.x86_64.rpm"
+
 
 # START: MAIN MENU
 
 OPTION=$(whiptail --title "GECOS Control Center Installation" --menu "Choose an option" 16 78 10 \
 "CHEF" "Install Chef server" \
 "CC" "Install GECOS Control Center." \
-"USER" "Create Control Center Superuser." \
+"USER" "Create Control Center First User." \
 "SET_SUPERUSER" "Set Control Center Superuser as Chef Superuser." \
 )
 
@@ -32,20 +35,33 @@ case $OPTION in
 
     
 CHEF)
+
     echo "INSTALLING CHEF SERVER"
+    echo "Please, check your FQDN, firewall, apparmor, ntp and mail configuration, before continuing"
+    apt update
+    apt install curl wget
+    aa-complain /etc/apparmor.d/*
+
+
     echo "Downloading package $CHEF_SERVER_PACKAGE_URL"
     curl -L "$CHEF_SERVER_PACKAGE_URL" > /tmp/chef-server.rpm
-    echo "Installing package"
-    rpm -Uvh --nosignature /tmp/chef-server.rpm
+    wget https://packages.chef.io/files/stable/chef-server/${CHEF_SERVER_VERSION}/ubuntu/18.04/chef-server-core_${CHEF_SERVER_VERSION}-1_amd64.deb
+    echo "Installing package"  
+    dpkg -i chef-server-core_${CHEF_SERVER_VERSION}-1_amd64.deb
     echo "Configuring"
+    chef-server-ctl reconfigure
+
+    echo "Creating an administrator account"
+    chef-server-ctl user-create $ADMIN_USER GECOS ADMIN $ADMIN_EMAIL '$ADMIN_PASSWORD' --filename /tmp/chefadmin.pem
+
     mkdir -p /etc/opscode/
     install_template "/etc/opscode/chef-server.rb" chef-server.rb 644 -subst
-    /opt/opscode/bin/chef-server-ctl reconfigure
+
     # Create the "default" organization
-    /opt/opscode/bin/chef-server-ctl org-create default default
-    echo "Opening port in Firewall"
-    lokkit -s https
+    chef-server-ctl org-create default default
+
     echo "CHEF SERVER INSTALLED"
+    echo "Please, move /tmp/chefadmin.pem to a safe place."
 ;;
 
 
@@ -55,7 +71,10 @@ echo "INSTALLING GECOS CONTROL CENTER"
 
 
 echo "Installing docker"
-apt install docker.io
+apt install docker.io docker-compose
+systemctl start docker
+docker-compose build
+# WARNING: If DNS resolution fails, you need to configure your DNS server properly in /etc/docker/daemon.json and then restart your Docker daemon. Please read: https://stackoverflow.com/questions/24991136/docker-build-could-not-resolve-archive-ubuntu-com-apt-get-fails-to-install-a/40516974#40516974
 
 echo "Installing GECOS Control Center UI"
 
@@ -64,7 +83,7 @@ echo "GECOS CONTROL CENTER INSTALLED"
 
 USER)
 echo "CREATING CONTROL CENTER FIRST USER"
-echo "(Pending)"
+docker exec -ti web pmanage gecoscc.ini create_adminuser --username $ADMIN_USER --email $ADMIN_EMAIL
 ;;
 
 
