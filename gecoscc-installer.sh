c2c2b03ae1e184d6a9246ac8abe8513bee9d7eee
a.macias.paredes <amacias@gruposolutia.com>
2020-07-02 10:33:04 +0200

    Update templates path

--- gecoscc-installer.sh
+++ gecoscc-installer.sh
@@ -40,7 +40,7 @@ export GECOSCC_VERSION='2.6.0'
 export GECOSCC_POLICIES_URL="https://github.com/gecos-team/gecos-workstation-management-cookbook/archive/0.11.2.zip"
 export GECOSCC_OHAI_URL="https://github.com/gecos-team/gecos-workstation-ohai-cookbook/archive/1.15.1.zip"
 export GECOSCC_URL="https://github.com/gecos-team/gecoscc-ui/archive/$GECOSCC_VERSION.zip"
-export TEMPLATES_URL="https://raw.githubusercontent.com/gecos-team/gecoscc-installer/2.5.0/templates"
+export TEMPLATES_URL="https://raw.githubusercontent.com/gecos-team/gecoscc-installer/2.6.0/templates"
 
 export NGINX_VERSION='1.4.3'
 
